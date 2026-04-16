eb421d3ba464ce352c760af341a6163f2c1d5db1
theonlyronsonik-lab <theonlyronsonik@gmail.com>
2026-04-16 15:39:44 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -61,7 +61,7 @@ last_div_time = {}  # {symbol: {"BULL": candle_dt_str, "BEAR": candle_dt_str}}
 signal_stack = {}
 
 SESSIONS = {
-    "Asia": (2, 10),
+    "Asia": (5, 7),
     "London": (7, 16),
     "New York": (13, 23),
 }
